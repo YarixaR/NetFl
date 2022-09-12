1b33ca288336921301df0aae454c83b4195ea6e0
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-09-12 15:49:18 -0400

    starter code

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,49 @@
+class ReviewsController < ApplicationController
+    before_action :find_review, only: [:show, :update, :destroy]
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
+  
+    # GET '/reviews'
+    def index
+      render json: Review.all, status: :ok
+    end
+  
+    # GET '/reviews/:id'
+    def show
+      render json: @review, status: :ok
+    end
+  
+    # POST '/reviews'
+    def create
+      review = Review.create!(review_params)
+      render json: review, status: :created
+    end
+  
+    # PATCH '/reviews/:id'
+    def update
+      @review.update!(review_params)
+      render json: @review, status: :ok
+    end
+  
+    # DELETE '/reviews/:id'
+    def destroy
+      @review.destroy
+      head :no_content
+    end
+  
+  
+    private
+  
+    def find_review
+      @review = Review.find(params[:id])
+    end
+  
+    def review_params
+      params.permit(:rating, :comment, :user_id, :movie_id)
+    end
+  
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
+  end
