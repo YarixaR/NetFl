e8aa9bdd7d9154c3a63c31f632666288af49e444
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-09-16 09:21:49 -0400

    merging

--- client/src/Components/Card.js
+++ client/src/Components/Card.js
@@ -10,7 +10,7 @@ function MovieCard({id, title, image, release, genre, description, trailer}) {
   }
 
   return (
-      <div id={'slider'} className='w-60 p-2 bg-black rounded-xl mt-10 flex flex-col overflow-hidden md:w-{240px} relative p-2' onClick={handleClick}>
+      <div className='w-60 p-2 bg-black rounded-xl mt-10 flex flex-col overflow-hidden md:w-{240px} relative p-2' onClick={handleClick}>
         <img className='rounded-xl w-full h-auto block' src={image} alt="movie"/>
         <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
           <h2 className='white-space-normal text-cs md:text-sm font-bold flex justify-center items-center h-full text-center'>{title}</h2>
